ad3e50bb8330120e57705522f390c6bbc5155c51
Balotelli99 <sultanavdi06@gmail.com>
2025-12-04 09:35:49 +0100

    4 december pop up

--- panorama.js
+++ panorama.js
@@ -1,12 +1,32 @@
-// Selecteer alle hotspots
-const hotspots = document.querySelectorAll('.hotspot');
+var popup = document.getElementById("popup");
+var popupTitle = document.getElementById("popup-title");
+var popupText = document.getElementById("popup-text");
+var popupImage = document.getElementById("popup-image");
+var popupClose = document.getElementById("popup-close");
 
-// Voeg klik-event toe aan elke hotspot
-hotspots.forEach(hotspot => {
-    hotspot.addEventListener('click', () => {
-        const uitleg = hotspot.querySelector('.hotspot-uitleg');
-        if (uitleg) {
-            uitleg.classList.toggle('show'); // toggle class 'show'
-        }
-    });
+// PAK ALLE HOTSPOTS
+var hotspots = document.querySelectorAll(".hotspot");
+
+// VOOR ELKE HOTSPOT
+hotspots.forEach(function (hotspot) {
+
+    hotspot.onclick = function () {
+
+        popup.style.display = "flex";
+
+        popupTitle.innerText = hotspot.getAttribute("data-title");
+        popupText.innerText = hotspot.getAttribute("data-text");
+        popupImage.src = hotspot.getAttribute("data-image");
+    };
+
 });
+
+// SLUITKNOP
+popupClose.onclick = function () {
+    popup.style.display = "none";
+};
+
+// KLIK BUITEN POPUP = SLUITEN
+popup.onclick = function () {
+    popup.style.display = "none";
+};
